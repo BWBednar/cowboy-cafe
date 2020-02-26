c5eb090a92a2bd56ed680f7adb29d8c8ed5b9e81
bwbednar <bwbednar@ksu.edu>
2020-02-26 14:48:31 -0600

    order tests added

---
+++ Data/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class Order
+    {
+        private static uint lastOrderNumber = 0;
+
+        private List<IOrderItem> items;
+
+        public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
+
+        public double Subtotal => 0;
+        
+        public uint OrderNumber { get => lastOrderNumber; }
+
+        public void Add(IOrderItem item)
+        {
+
+        }
+
+        public void Remove(IOrderItem item)
+        {
+
+        }
+    }
+}
